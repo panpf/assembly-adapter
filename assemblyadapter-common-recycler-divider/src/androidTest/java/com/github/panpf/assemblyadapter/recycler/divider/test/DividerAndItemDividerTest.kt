900e5700d7f4200603f2436337fae98c9e44ed8f
panpf <panpfpanpf@outlook.com>
2021-08-26 18:58:05 +0800

    test: Complete the unit test of the common-recycler-divider module

---
+++ assemblyadapter-common-recycler-divider/src/androidTest/java/com/github/panpf/assemblyadapter/recycler/divider/test/DividerAndItemDividerTest.kt
@@ -0,0 +1,157 @@
+/*
+ * Copyright (C) 2021 panpf <panpfpanpf@outlook.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.panpf.assemblyadapter.recycler.divider.test
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import androidx.core.content.res.ResourcesCompat
+import androidx.test.platform.app.InstrumentationRegistry
+import com.github.panpf.assemblyadapter.common.recycler.divider.test.R
+import com.github.panpf.assemblyadapter.recycler.divider.Divider
+import com.github.panpf.assemblyadapter.recycler.divider.Insets
+import org.junit.Assert
+import org.junit.Test
+
+class DividerAndItemDividerTest {
+
+    @Test
+    fun testColor() {
+        val context = InstrumentationRegistry.getInstrumentation().context
+
+        /**
+         * color
+         */
+        Divider.color(Color.RED, 5).toItemDivider(context).apply {
+            val colorDrawable = this.drawable as ColorDrawable
+            Assert.assertEquals(Color.RED, colorDrawable.color)
+        }
+        Divider.color(Color.BLUE, 5).toItemDivider(context).apply {
+            val colorDrawable = this.drawable as ColorDrawable
+            Assert.assertEquals(Color.BLUE, colorDrawable.color)
+        }
+
+        /**
+         * size
+         */
+        Divider.color(Color.RED, 10).toItemDivider(context).apply {
+            Assert.assertEquals(10, drawableWidthSize)
+            Assert.assertEquals(10, drawableHeightSize)
+            Assert.assertEquals(10, widthSize)
+            Assert.assertEquals(10, heightSize)
+        }
+        Divider.color(Color.RED, 20).toItemDivider(context).apply {
+            Assert.assertEquals(20, drawableWidthSize)
+            Assert.assertEquals(20, drawableHeightSize)
+            Assert.assertEquals(20, widthSize)
+            Assert.assertEquals(20, heightSize)
+        }
+
+        /**
+         * inset size
+         */
+        Divider.color(Color.RED, 20, Insets.of(1, 2, 3, 4)).toItemDivider(context).apply {
+            Assert.assertEquals(1, insetStart)
+            Assert.assertEquals(2, insetTop)
+            Assert.assertEquals(3, insetEnd)
+            Assert.assertEquals(4, insetBottom)
+            Assert.assertEquals(4, insetWidthSize)
+            Assert.assertEquals(6, insetHeightSize)
+            Assert.assertEquals(20, drawableWidthSize)
+            Assert.assertEquals(20, drawableHeightSize)
+            Assert.assertEquals(24, widthSize)
+            Assert.assertEquals(26, heightSize)
+        }
+        Divider.color(Color.RED, 20, Insets.of(4, 5, 6, 7)).toItemDivider(context).apply {
+            Assert.assertEquals(4, insetStart)
+            Assert.assertEquals(5, insetTop)
+            Assert.assertEquals(6, insetEnd)
+            Assert.assertEquals(7, insetBottom)
+            Assert.assertEquals(10, insetWidthSize)
+            Assert.assertEquals(12, insetHeightSize)
+            Assert.assertEquals(20, drawableWidthSize)
+            Assert.assertEquals(20, drawableHeightSize)
+            Assert.assertEquals(30, widthSize)
+            Assert.assertEquals(32, heightSize)
+        }
+    }
+
+    @Test
+    fun testColorRes() {
+        val context = InstrumentationRegistry.getInstrumentation().context
+
+        /**
+         * color
+         */
+        Divider.colorRes(R.color.test1, 5).toItemDivider(context).apply {
+            val colorDrawable = this.drawable as ColorDrawable
+            Assert.assertEquals(
+                ResourcesCompat.getColor(context.resources, R.color.test1, null),
+                colorDrawable.color
+            )
+        }
+        Divider.colorRes(R.color.test2, 5).toItemDivider(context).apply {
+            val colorDrawable = this.drawable as ColorDrawable
+            Assert.assertEquals(
+                ResourcesCompat.getColor(context.resources, R.color.test2, null),
+                colorDrawable.color
+            )
+        }
+
+        /**
+         * size
+         */
+        Divider.colorRes(R.color.test1, 10).toItemDivider(context).apply {
+            Assert.assertEquals(10, drawableWidthSize)
+            Assert.assertEquals(10, drawableHeightSize)
+            Assert.assertEquals(10, widthSize)
+            Assert.assertEquals(10, heightSize)
+        }
+        Divider.colorRes(R.color.test1, 20).toItemDivider(context).apply {
+            Assert.assertEquals(20, drawableWidthSize)
+            Assert.assertEquals(20, drawableHeightSize)
+            Assert.assertEquals(20, widthSize)
+            Assert.assertEquals(20, heightSize)
+        }
+
+        /**
+         * inset size
+         */
+        Divider.colorRes(R.color.test1, 20, Insets.of(1, 2, 3, 4)).toItemDivider(context).apply {
+            Assert.assertEquals(1, insetStart)
+            Assert.assertEquals(2, insetTop)
+            Assert.assertEquals(3, insetEnd)
+            Assert.assertEquals(4, insetBottom)
+            Assert.assertEquals(4, insetWidthSize)
+            Assert.assertEquals(6, insetHeightSize)
+            Assert.assertEquals(20, drawableWidthSize)
+            Assert.assertEquals(20, drawableHeightSize)
+            Assert.assertEquals(24, widthSize)
+            Assert.assertEquals(26, heightSize)
+        }
+        Divider.colorRes(R.color.test1, 20, Insets.of(4, 5, 6, 7)).toItemDivider(context).apply {
+            Assert.assertEquals(4, insetStart)
+            Assert.assertEquals(5, insetTop)
+            Assert.assertEquals(6, insetEnd)
+            Assert.assertEquals(7, insetBottom)
+            Assert.assertEquals(10, insetWidthSize)
+            Assert.assertEquals(12, insetHeightSize)
+            Assert.assertEquals(20, drawableWidthSize)
+            Assert.assertEquals(20, drawableHeightSize)
+            Assert.assertEquals(30, widthSize)
+            Assert.assertEquals(32, heightSize)
+        }
+    }
+}
---
+++ assemblyadapter-common-recycler-divider/src/androidTest/java/com/github/panpf/assemblyadapter/recycler/divider/test/InsetsTest.kt
@@ -0,0 +1,78 @@
+/*
+ * Copyright (C) 2021 panpf <panpfpanpf@outlook.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.panpf.assemblyadapter.recycler.divider.test
+
+import com.github.panpf.assemblyadapter.recycler.divider.Insets
+import org.junit.Assert
+import org.junit.Test
+
+class InsetsTest {
+
+    @Test
+    fun test() {
+        Insets.of(1, 2, 3, 4).apply {
+            Assert.assertEquals(1, start)
+            Assert.assertEquals(2, top)
+            Assert.assertEquals(3, end)
+            Assert.assertEquals(4, bottom)
+        }
+        Insets.of(4, 5, 6, 7).apply {
+            Assert.assertEquals(4, start)
+            Assert.assertEquals(5, top)
+            Assert.assertEquals(6, end)
+            Assert.assertEquals(7, bottom)
+        }
+
+        Insets.startAndEndOf(2).apply {
+            Assert.assertEquals(2, start)
+            Assert.assertEquals(0, top)
+            Assert.assertEquals(2, end)
+            Assert.assertEquals(0, bottom)
+        }
+        Insets.startAndEndOf(4).apply {
+            Assert.assertEquals(4, start)
+            Assert.assertEquals(0, top)
+            Assert.assertEquals(4, end)
+            Assert.assertEquals(0, bottom)
+        }
+
+        Insets.topAndBottomOf(2).apply {
+            Assert.assertEquals(0, start)
+            Assert.assertEquals(2, top)
+            Assert.assertEquals(0, end)
+            Assert.assertEquals(2, bottom)
+        }
+        Insets.topAndBottomOf(4).apply {
+            Assert.assertEquals(0, start)
+            Assert.assertEquals(4, top)
+            Assert.assertEquals(0, end)
+            Assert.assertEquals(4, bottom)
+        }
+
+        Insets.allOf(2).apply {
+            Assert.assertEquals(2, start)
+            Assert.assertEquals(2, top)
+            Assert.assertEquals(2, end)
+            Assert.assertEquals(2, bottom)
+        }
+        Insets.allOf(4).apply {
+            Assert.assertEquals(4, start)
+            Assert.assertEquals(4, top)
+            Assert.assertEquals(4, end)
+            Assert.assertEquals(4, bottom)
+        }
+    }
+}
