b22299f957a4893eaa1abbd1a91db23569a5437b
panpf <panpfpanpf@outlook.com>
2021-09-03 18:54:09 +0800

    test: Supplement the unit test of the pager module

--- assemblyadapter-pager/src/androidTest/java/com/github/panpf/assemblyadapter/pager/test/refreshable/RefreshableFragmentStatePagerAdapterTest.kt
+++ assemblyadapter-pager/src/androidTest/java/com/github/panpf/assemblyadapter/pager/test/refreshable/RefreshableFragmentStatePagerAdapterTest.kt
@@ -15,7 +15,221 @@
  */
 package com.github.panpf.assemblyadapter.pager.test.refreshable
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.viewpager.widget.PagerAdapter
+import com.github.panpf.assemblyadapter.pager.R
+import com.github.panpf.assemblyadapter.pager.refreshable.RefreshableFragmentStatePagerAdapter
+import com.github.panpf.tools4a.test.ktx.getFragmentSync
+import com.github.panpf.tools4a.test.ktx.launchFragmentInContainer
+import com.github.panpf.tools4j.test.ktx.assertThrow
+import org.junit.Assert
+import org.junit.Test
+
 class RefreshableFragmentStatePagerAdapterTest {
 
-    // todo Supplementary test
+    private class TestRefreshableFragmentStatePagerAdapter(
+        fragmentManager: FragmentManager, var list: List<String>
+    ) : RefreshableFragmentStatePagerAdapter<String>(fragmentManager) {
+
+        override fun getItemData(position: Int): String {
+            val count = count
+            if (position < 0 || position >= count) {
+                throw IndexOutOfBoundsException("Index: $position, Size: $count")
+            }
+            return list[position]
+        }
+
+        override fun getCount(): Int = list.size
+
+        override fun getFragment(position: Int): Fragment {
+            return TestFragment.create(getItemData(position))
+        }
+    }
+
+    class TestFragment : Fragment() {
+
+        companion object {
+            fun create(data: String) = TestFragment().apply {
+                arguments = Bundle().apply {
+                    putString("data", data)
+                }
+            }
+        }
+
+        override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ): View = TextView(requireContext()).apply {
+            text = arguments!!.getString("data")
+        }
+    }
+
+    class ContainerFragment : Fragment()
+
+    @Test
+    fun testPropertyIsDisableItemRefreshWhenDataSetChanged() {
+        val fragmentScenario = ContainerFragment::class.launchFragmentInContainer()
+        val fragment = fragmentScenario.getFragmentSync()
+        TestRefreshableFragmentStatePagerAdapter(
+            fragment.childFragmentManager,
+            listOf("hello", "world")
+        ).apply {
+            Assert.assertFalse(isDisableItemRefreshWhenDataSetChanged)
+
+            isDisableItemRefreshWhenDataSetChanged = true
+            Assert.assertTrue(isDisableItemRefreshWhenDataSetChanged)
+
+            isDisableItemRefreshWhenDataSetChanged = false
+            Assert.assertFalse(isDisableItemRefreshWhenDataSetChanged)
+        }
+    }
+
+    @Test
+    fun testMethodGetItem() {
+        val fragmentScenario = ContainerFragment::class.launchFragmentInContainer()
+        val fragment = fragmentScenario.getFragmentSync()
+
+        TestRefreshableFragmentStatePagerAdapter(
+            fragment.childFragmentManager,
+            listOf("hello", "world")
+        ).apply {
+            assertThrow(IndexOutOfBoundsException::class) {
+                getItem(-1)
+            }
+            var item0: TestFragment? = null
+            fragmentScenario.onFragment {
+                item0 = (getItem(0) as TestFragment)
+            }
+            item0!!.apply {
+                Assert.assertNull(view)
+                fragmentScenario.onFragment {
+                    fragment.childFragmentManager.beginTransaction()
+                        .add(this, null)
+                        .commit()
+                }
+                Thread.sleep(100)
+                (view as TextView).apply {
+                    Assert.assertEquals("hello", text.toString())
+                    Assert.assertEquals(0, (getTag(R.id.aa_tag_pager_refresh_position) as Int))
+                    Assert.assertEquals("hello", (getTag(R.id.aa_tag_pager_refresh_data)))
+                }
+            }
+
+            var item1: TestFragment? = null
+            fragmentScenario.onFragment {
+                item1 = (getItem(1) as TestFragment)
+            }
+            item1!!.apply {
+                Assert.assertNull(view)
+                fragmentScenario.onFragment {
+                    fragment.childFragmentManager.beginTransaction()
+                        .add(this, null)
+                        .commit()
+                }
+                Thread.sleep(100)
+                (view as TextView).apply {
+                    Assert.assertEquals("world", text.toString())
+                    Assert.assertEquals(1, (getTag(R.id.aa_tag_pager_refresh_position) as Int))
+                    Assert.assertEquals("world", (getTag(R.id.aa_tag_pager_refresh_data)))
+                }
+            }
+
+            isDisableItemRefreshWhenDataSetChanged = true
+            item0 = null
+            fragmentScenario.onFragment {
+                item0 = (getItem(0) as TestFragment)
+            }
+            item0!!.apply {
+                Assert.assertNull(view)
+                fragmentScenario.onFragment {
+                    fragment.childFragmentManager.beginTransaction()
+                        .add(this, null)
+                        .commit()
+                }
+                Thread.sleep(100)
+                (view as TextView).apply {
+                    Assert.assertEquals("hello", text.toString())
+                    Assert.assertNull(getTag(R.id.aa_tag_pager_refresh_position))
+                    Assert.assertNull(getTag(R.id.aa_tag_pager_refresh_data))
+                }
+            }
+
+            item1 = null
+            fragmentScenario.onFragment {
+                item1 = (getItem(1) as TestFragment)
+            }
+            item1!!.apply {
+                Assert.assertNull(view)
+                fragmentScenario.onFragment {
+                    fragment.childFragmentManager.beginTransaction()
+                        .add(this, null)
+                        .commit()
+                }
+                Thread.sleep(100)
+                (view as TextView).apply {
+                    Assert.assertEquals("world", text.toString())
+                    Assert.assertNull(getTag(R.id.aa_tag_pager_refresh_position))
+                    Assert.assertNull(getTag(R.id.aa_tag_pager_refresh_data))
+                }
+            }
+        }
+    }
+
+    @Test
+    fun testMethodGetItemPosition() {
+        val fragmentScenario = ContainerFragment::class.launchFragmentInContainer()
+        val fragment = fragmentScenario.getFragmentSync()
+        TestRefreshableFragmentStatePagerAdapter(
+            fragment.childFragmentManager,
+            listOf("hello", "world")
+        ).apply {
+            var item0: TestFragment? = null
+            fragmentScenario.onFragment {
+                item0 = (getItem(0) as TestFragment)
+            }
+            var item1: TestFragment? = null
+            fragmentScenario.onFragment {
+                item1 = (getItem(1) as TestFragment)
+            }
+            Assert.assertNull(item0!!.view)
+            Assert.assertNull(item1!!.view)
+            Assert.assertEquals(PagerAdapter.POSITION_NONE, getItemPosition(item0!!))
+            Assert.assertEquals(PagerAdapter.POSITION_NONE, getItemPosition(item1!!))
+
+            fragmentScenario.onFragment {
+                fragment.childFragmentManager.beginTransaction()
+                    .add(item0!!, null)
+                    .add(item1!!, null)
+                    .commit()
+            }
+            Thread.sleep(100)
+            Assert.assertEquals(PagerAdapter.POSITION_UNCHANGED, getItemPosition(item0!!))
+            Assert.assertEquals(PagerAdapter.POSITION_UNCHANGED, getItemPosition(item1!!))
+
+            list = listOf("good", "world")
+            Assert.assertEquals(PagerAdapter.POSITION_NONE, getItemPosition(item0!!))
+            Assert.assertEquals(PagerAdapter.POSITION_UNCHANGED, getItemPosition(item1!!))
+
+            isDisableItemRefreshWhenDataSetChanged = true
+            Assert.assertEquals(PagerAdapter.POSITION_UNCHANGED, getItemPosition(item0!!))
+            Assert.assertEquals(PagerAdapter.POSITION_UNCHANGED, getItemPosition(item1!!))
+            isDisableItemRefreshWhenDataSetChanged = false
+
+            list = listOf("hello", "good")
+            Assert.assertEquals(PagerAdapter.POSITION_UNCHANGED, getItemPosition(item0!!))
+            Assert.assertEquals(PagerAdapter.POSITION_NONE, getItemPosition(item1!!))
+
+            isDisableItemRefreshWhenDataSetChanged = true
+            Assert.assertEquals(PagerAdapter.POSITION_UNCHANGED, getItemPosition(item0!!))
+            Assert.assertEquals(PagerAdapter.POSITION_UNCHANGED, getItemPosition(item1!!))
+            isDisableItemRefreshWhenDataSetChanged = false
+        }
+    }
 }
--- assemblyadapter-pager/src/androidTest/java/com/github/panpf/assemblyadapter/pager/test/refreshable/RefreshablePagerAdapterTest.kt
+++ assemblyadapter-pager/src/androidTest/java/com/github/panpf/assemblyadapter/pager/test/refreshable/RefreshablePagerAdapterTest.kt
@@ -15,7 +15,143 @@
  */
 package com.github.panpf.assemblyadapter.pager.test.refreshable
 
+import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.TextView
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.viewpager.widget.PagerAdapter
+import com.github.panpf.assemblyadapter.pager.R
+import com.github.panpf.assemblyadapter.pager.refreshable.RefreshablePagerAdapter
+import com.github.panpf.tools4j.test.ktx.assertThrow
+import org.junit.Assert
+import org.junit.Test
+
 class RefreshablePagerAdapterTest {
 
-    // todo Supplementary test
+    private class TestRefreshablePagerAdapter(var list: List<String>) :
+        RefreshablePagerAdapter<String>() {
+
+        override fun getItemData(position: Int): String {
+            val count = count
+            if (position < 0 || position >= count) {
+                throw IndexOutOfBoundsException("Index: $position, Size: $count")
+            }
+            return list[position]
+        }
+
+        override fun getCount(): Int = list.size
+
+        override fun getView(container: ViewGroup, position: Int): View {
+            return TextView(container.context).apply {
+                text = getItemData(position)
+            }
+        }
+    }
+
+    @Test
+    fun testPropertyIsDisableItemRefreshWhenDataSetChanged() {
+        TestRefreshablePagerAdapter(listOf("hello", "world")).apply {
+            Assert.assertFalse(isDisableItemRefreshWhenDataSetChanged)
+
+            isDisableItemRefreshWhenDataSetChanged = true
+            Assert.assertTrue(isDisableItemRefreshWhenDataSetChanged)
+
+            isDisableItemRefreshWhenDataSetChanged = false
+            Assert.assertFalse(isDisableItemRefreshWhenDataSetChanged)
+        }
+    }
+
+    @Test
+    fun testMethodInstantiateItem() {
+        val context = InstrumentationRegistry.getInstrumentation().context
+        val container = FrameLayout(context)
+
+        TestRefreshablePagerAdapter(listOf("hello", "world")).apply {
+            assertThrow(IndexOutOfBoundsException::class) {
+                instantiateItem(container, -1)
+            }
+            (instantiateItem(container, 0) as TextView).apply {
+                Assert.assertEquals("hello", text.toString())
+                Assert.assertEquals(0, (getTag(R.id.aa_tag_pager_refresh_position) as Int))
+                Assert.assertEquals("hello", (getTag(R.id.aa_tag_pager_refresh_data)))
+            }
+            (instantiateItem(container, 1) as TextView).apply {
+                Assert.assertEquals("world", text.toString())
+                Assert.assertEquals(1, (getTag(R.id.aa_tag_pager_refresh_position) as Int))
+                Assert.assertEquals("world", (getTag(R.id.aa_tag_pager_refresh_data)))
+            }
+            assertThrow(IndexOutOfBoundsException::class) {
+                instantiateItem(container, 2)
+            }
+
+            isDisableItemRefreshWhenDataSetChanged = true
+            (instantiateItem(container, 0) as TextView).apply {
+                Assert.assertEquals("hello", text.toString())
+                Assert.assertNull(getTag(R.id.aa_tag_pager_refresh_position))
+                Assert.assertNull(getTag(R.id.aa_tag_pager_refresh_data))
+            }
+            (instantiateItem(container, 1) as TextView).apply {
+                Assert.assertEquals("world", text.toString())
+                Assert.assertNull(getTag(R.id.aa_tag_pager_refresh_position))
+                Assert.assertNull(getTag(R.id.aa_tag_pager_refresh_data))
+            }
+        }
+    }
+
+    @Test
+    fun testMethodDestroyItem() {
+        val context = InstrumentationRegistry.getInstrumentation().context
+        val container = FrameLayout(context)
+        TestRefreshablePagerAdapter(listOf("hello", "world")).apply {
+            assertThrow(IndexOutOfBoundsException::class) {
+                destroyItem(container, -1, "")
+            }
+            destroyItem(container, 0, instantiateItem(container, 0))
+            destroyItem(container, 1, instantiateItem(container, 1))
+            assertThrow(IndexOutOfBoundsException::class) {
+                destroyItem(container, 2, "")
+            }
+        }
+    }
+
+    @Test
+    fun testMethodIsViewFromObject() {
+        val context = InstrumentationRegistry.getInstrumentation().context
+        val view = TextView(context)
+        TestRefreshablePagerAdapter(listOf("hello", "world")).apply {
+            Assert.assertTrue(isViewFromObject(view, view))
+            Assert.assertFalse(isViewFromObject(view, ""))
+        }
+    }
+
+    @Test
+    fun testMethodGetItemPosition() {
+        val context = InstrumentationRegistry.getInstrumentation().context
+        val container = FrameLayout(context)
+        TestRefreshablePagerAdapter(listOf("hello", "world")).apply {
+            val item0 = instantiateItem(container, 0)
+            val item1 = instantiateItem(container, 1)
+            Assert.assertEquals(PagerAdapter.POSITION_UNCHANGED, getItemPosition(item0))
+            Assert.assertEquals(PagerAdapter.POSITION_UNCHANGED, getItemPosition(item1))
+
+            list = listOf("good", "world")
+            Assert.assertEquals(PagerAdapter.POSITION_NONE, getItemPosition(item0))
+            Assert.assertEquals(PagerAdapter.POSITION_UNCHANGED, getItemPosition(item1))
+
+            isDisableItemRefreshWhenDataSetChanged = true
+            Assert.assertEquals(PagerAdapter.POSITION_UNCHANGED, getItemPosition(item0))
+            Assert.assertEquals(PagerAdapter.POSITION_UNCHANGED, getItemPosition(item1))
+            isDisableItemRefreshWhenDataSetChanged = false
+
+            list = listOf("hello", "good")
+            Assert.assertEquals(PagerAdapter.POSITION_UNCHANGED, getItemPosition(item0))
+            Assert.assertEquals(PagerAdapter.POSITION_NONE, getItemPosition(item1))
+
+            isDisableItemRefreshWhenDataSetChanged = true
+            Assert.assertEquals(PagerAdapter.POSITION_UNCHANGED, getItemPosition(item0))
+            Assert.assertEquals(PagerAdapter.POSITION_UNCHANGED, getItemPosition(item1))
+            isDisableItemRefreshWhenDataSetChanged = false
+        }
+    }
 }
