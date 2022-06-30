d6c629844d9cc121d6266c1bf456205cc4fbb000
panpf <panpfpanpf@outlook.com>
2022-06-30 22:47:22 +0800

    upgrade: Upgrade sketch 3.0.0-beta04

--- sample/src/main/java/com/github/panpf/assemblyadapter/sample/MyApplication.kt
+++ sample/src/main/java/com/github/panpf/assemblyadapter/sample/MyApplication.kt
@@ -2,16 +2,16 @@ package com.github.panpf.assemblyadapter.sample
 
 import androidx.multidex.MultiDexApplication
 import com.github.panpf.sketch.Sketch
-import com.github.panpf.sketch.SketchConfigurator
+import com.github.panpf.sketch.SketchFactory
 import com.github.panpf.sketch.decode.AppIconBitmapDecoder
 import com.github.panpf.sketch.fetch.AppIconUriFetcher
 
-class MyApplication : MultiDexApplication(), SketchConfigurator {
+class MyApplication : MultiDexApplication(), SketchFactory {
 
-    override fun createSketchConfig(): Sketch.Builder.() -> Unit = {
-        components {
-            addFetcher(AppIconUriFetcher.Factory())
-            addBitmapDecoder(AppIconBitmapDecoder.Factory())
-        }
-    }
+    override fun createSketch(): Sketch =
+        Sketch.Builder(this)
+            .components {
+                addFetcher(AppIconUriFetcher.Factory())
+                addBitmapDecoder(AppIconBitmapDecoder.Factory())
+            }.build()
 }
